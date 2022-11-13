0bf8cd0052f6559f74d828ef6ddd24e6a802eec5
azz94 <zalexandra994@gmail.com>
2022-11-13 18:48:08 +0100

    29-js-sumazmiennych

---
+++ js/29-js-suma-zmiennych.js
@@ -0,0 +1,17 @@
+let firstNumber = 7;
+
+let secondNumber = 26;
+
+function addition(firstNumber, secondNumber) {
+    let result = wynik(firstNumber + secondNumber);
+
+    return result;
+}
+
+// console.log( addition(firstNumber, secondNumber) );
+
+let wynik = firstNumber + secondNumber;
+
+// let wynik = addition(firstNumber, secondNumber);
+
+console.log(wynik);
